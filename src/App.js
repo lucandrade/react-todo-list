11a1c14e327886b3ec0c04d8c739d340592cae03
Lucas Andrade Oliveira <lucasandradedeveloper@gmail.com>
2016-11-10 15:11:54 -0200

    Adding menu component, adjusting menu style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+'use strict';
+
 import React, { Component } from 'react';
-import { Link } from 'react-router';
+import Menu from './components/Menu';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,12 +12,7 @@ class App extends Component {
         <div className="App-header">
           <h2>React Todo List</h2>
           <div className="App-intro">
-            <ul className="App-menu">
-              <li>
-                <Link to='/local-storage' activeClassName='active'>Local Storage</Link>
-                <Link to='/firebase' activeClassName='active'>Firebase</Link>
-              </li>
-            </ul>
+            <Menu />
           </div>
         </div>
         <div className="container">
---
+++ src/components/Menu.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class Menu extends React.Component {
+	render() {
+		return(
+			<ul className="App-menu">
+        <li>
+          <Link to='/local-storage' activeClassName='active'>Local Storage</Link>
+          <Link to='/firebase' activeClassName='active'>Firebase</Link>
+        </li>
+      </ul>
+		);
+	}
+}
